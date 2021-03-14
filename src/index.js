e0cdbb27baae19e731b2323939ab6108c76a624d
Pavel Tselyuk <pavlik_astap@mail.ru>
2021-03-14 23:45:57 +0300

    feat: task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let num = (String(n).split('')); //.reverse().join(''));
+    if (num[0] == '-') { num.splice(0, 1); }
+
+    return +(num.reverse().join(''));
 }
